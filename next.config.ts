d284b8dc0e964eecbb641581854ff1bc2fc66398
Özgür Özdemir <gelenevrak@gmail.com>
2026-06-02 16:42:24 +0200

    chore: Configure allowedDevOrigins in next.config.ts to resolve HMR mobile access blocks

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["192.168.100.16", "192.168.100.16:3001", "localhost:3001"]
 };
 
 export default nextConfig;
